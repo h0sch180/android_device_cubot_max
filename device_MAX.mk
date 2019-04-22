6879d856feecb0a8c2f24e0f9fe47b4526133916
root <root@ANUBIS.anukis.local>
2019-04-22 15:00:00 +0200

    correct some filenames

--- device_MAX.mk
+++ device_MAX.mk
@@ -144,13 +144,13 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/root/fstab.mt6753:root/fstab.mt6753 \
-    $(LOCAL_PATH)/rootdir/root/init.mt6753.rc:root/init.mt6753.rc \
+    $(LOCAL_PATH)/rootdir/root/fstab.mt6735:root/fstab.mt6735 \
+    $(LOCAL_PATH)/rootdir/root/init.mt6735.rc:root/init.mt6735.rc \
     $(LOCAL_PATH)/rootdir/root/init.modem.rc:root/init.modem.rc \
-    $(LOCAL_PATH)/rootdir/root/init.recovery.mt6753.rc:root/init.recovery.mt6753.rc \
-    $(LOCAL_PATH)/rootdir/root/init.mt6753.usb.rc:root/init.mt6753.usb.rc \
+    $(LOCAL_PATH)/rootdir/root/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc \
+    $(LOCAL_PATH)/rootdir/root/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
     $(LOCAL_PATH)/rootdir/root/twrp.fstab:recovery/root/etc/twrp.fstab \
-    $(LOCAL_PATH)/rootdir/root/ueventd.mt6753.rc:root/ueventd.mt6753.rc \
+    $(LOCAL_PATH)/rootdir/root/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
     $(LOCAL_PATH)/rootdir/root/init.project.rc:root/init.project.rc
 
 # Permissions
