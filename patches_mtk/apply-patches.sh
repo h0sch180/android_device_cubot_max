336af4c16f9f27f58dbeec630f649a888ed39fa2
seluce <36071735+seluce@users.noreply.github.com>
2019-04-24 20:39:18 +0200

    fix compile error with appöy-patches
    - removed some git clean commits to fix compile error at 0%
    - it stays on settings to fix compile error later on 88%
    
    verified the patches now and the script runs now without issues

--- patches_mtk/apply-patches.sh
+++ patches_mtk/apply-patches.sh
@@ -1,51 +1,39 @@
 #!/bin/bash
 #cd ../../../..
 cd system/core
-patch -t -p1 < ../../device/CUBOT/MAX/patches_mtk/system_core.patch
-git clean -f -d
+patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_core.patch
 cd ../..
 cd bionic
-patch -t -p1 < ../device/CUBOT/MAX/patches_mtk/bionic.patch
-git clean -f -d
+patch -p1 < ../device/CUBOT/MAX/patches_mtk/bionic.patch
 cd ..
 cd system/sepolicy
-patch -t -p1 < ../../device/CUBOT/MAX/patches_mtk/system_sepolicy.patch
-git clean -f -d
+patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_sepolicy.patch
 cd ../..
 cd frameworks/av
-patch -t -p1 < ../../device/CUBOT/MAX/patches_mtk/frameworks_av.patch
-git clean -f -d
+patch -p1 < ../../device/CUBOT/MAX/patches_mtk/frameworks_av.patch
 cd ..
 cd native
-patch -t -p1 < ../../device/CUBOT/MAX/patches_mtk/frameworks_native.patch
-git clean -f -d
+patch -p1 < ../../device/CUBOT/MAX/patches_mtk/frameworks_native.patch
 cd ..
 cd base
-patch -t -p1 < ../../device/CUBOT/MAX/patches_mtk/frameworks_base.patch
-git clean -f -d
+patch -p1 < ../../device/CUBOT/MAX/patches_mtk/frameworks_base.patch
 cd ..
 cd opt/telephony
-patch -t -p1 < ../../../device/CUBOT/MAX/patches_mtk/frameworks_opt_telephony.patch
-git clean -f -d
+patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/frameworks_opt_telephony.patch
 cd ../../..
 cd packages/apps/Snap
-patch -t -p1 < ../../../device/CUBOT/MAX/patches_mtk/snap.patch
-git clean -f -d
+patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/snap.patch
 cd ..
 cd FMRadio
-patch -t -p1 < ../../../device/CUBOT/MAX/patches_mtk/fmradio.patch
-git clean -f -d
+patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/fmradio.patch
 cd ../../..
 cd system/netd
-patch -t -p1 < ../../device/CUBOT/MAX/patches_mtk/system_netd.patch
-git clean -f -d
+patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_netd.patch
 cd ../..
 cd vendor/cmsdk
-patch -t -p1 < ../../device/CUBOT/MAX/patches_mtk/vendor_cmsdk.patch
-git clean -f -d
+patch -p1 < ../../device/CUBOT/MAX/patches_mtk/vendor_cmsdk.patch
 cd ../..
 cd packages/apps/Settings
-patch -t -p1 < ../../../device/CUBOT/MAX/patches_mtk/settings.patch
+patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/settings.patch
 git clean -f -d
 cd ../../..
-
