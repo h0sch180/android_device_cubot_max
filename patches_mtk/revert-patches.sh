48295f81b1296cf30b5c1c7753a5e99091ff49f2
seluce <36071735+seluce@users.noreply.github.com>
2019-04-27 17:40:05 +0200

    Create revert-patches.sh

---
+++ patches_mtk/revert-patches.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#cd ../../../..
+cd system/core
+git reset --hard && git clean -fd
+cd ../..
+cd bionic
+git reset --hard && git clean -fd
+cd ..
+cd system/sepolicy
+git reset --hard && git clean -fd
+cd ../..
+cd frameworks/av
+git reset --hard && git clean -fd
+cd ..
+cd native
+git reset --hard && git clean -fd
+cd ..
+cd base
+git reset --hard && git clean -fd
+cd ..
+cd opt/telephony
+git reset --hard && git clean -fd
+cd ../../..
+cd packages/apps/Snap
+git reset --hard && git clean -fd
+cd ..
+cd FMRadio
+git reset --hard && git clean -fd
+cd ../../..
+cd system/netd
+git reset --hard && git clean -fd
+cd ../..
+cd packages/apps/Settings
+git reset --hard && git clean -fd
+cd ../../..
