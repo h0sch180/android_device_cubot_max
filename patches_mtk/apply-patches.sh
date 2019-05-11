8f9b55d2baf42b5bad858d1a7b274f3bdc237539
h0sch180 <hoschi@anukis.de>
2019-05-11 23:21:50 +0200

    clean patches

--- patches_mtk/apply-patches.sh
+++ patches_mtk/apply-patches.sh
@@ -1,61 +1,64 @@
 #!/bin/bash
+
+brand="`echo $0 | cut -f2 -d'/'`"
+model="`echo $0 | cut -f3 -d'/'`"
+
 #cd ../../../..
 cd system/core
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_core/0001-system_core.patch
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_core/0002-Prevent-spamming-audio.patch
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_core/0003-healthd-correctly-report-battery-voltage-on-MTK-kernels.patch
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_core/0004_system_core_rootdir_ueventd.patch
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_core/0005_system_core_fingerprint.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/system_core/0001-system_core.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/system_core/0002-Prevent-spamming-audio.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/system_core/0003-healthd-correctly-report-battery-voltage-on-MTK-kernels.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/system_core/0004_system_core_rootdir_ueventd.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/system_core/0005_system_core_fingerprint.patch
 cd ../..
 cd bionic
-patch -p1 < ../device/CUBOT/MAX/patches_mtk/bionic.patch
+patch -p1 < ../device/$brand/$model/patches_mtk/bionic.patch
 cd ..
 cd system/sepolicy
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_sepolicy.patch
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_sepolicy2.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/system_sepolicy.patch
 cd ../..
 cd system/netd
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_netd.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/system_netd.patch
 cd ../..
 
 ### DO NOT USE SYSTEM BLUETOOTH PATCH
 #cd system/bt
-#patch -p1 < ../../device/CUBOT/MAX/patches_mtk/system_bt.patch
+#patch -p1 < ../../device/$brand/$model/patches_mtk/system_bt.patch
 #cd ../..
 ### DO NOT USE SYSTEM BLUETOOTH PATCH
 
 cd frameworks/av
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/frameworks_av.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/frameworks_av.patch
 cd ..
 cd native
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/frameworks_native.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/frameworks_native.patch
 cd ..
 cd base
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/framework_base/0001-frameworks_base.patch
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/framework_base/0002-Fix-Minior-Bugs.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/framework_base/0001-frameworks_base.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/framework_base/0002-Fix-Minior-Bugs.patch
 cd ..
 cd opt/telephony
-patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/frameworks_opt_telephony.patch
+patch -p1 < ../../../device/$brand/$model/patches_mtk/frameworks_opt_telephony.patch
 cd ../../..
 cd packages/apps/Snap
-patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/snap.patch
+patch -p1 < ../../../device/$brand/$model/patches_mtk/snap.patch
 cd ..
 cd FMRadio
-patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/fmradio.patch
+patch -p1 < ../../../device/$brand/$model/patches_mtk/fmradio.patch
 cd ../../..
+cd external/wpa_supplicant_8
+patch -p1 < ../../device/$brand/$model/patches_mtk/external_wpa_supplicant_8/0001-Ignore-fake-nvram-wifi.patch
+cd ../..
 cd packages/apps/Settings
-patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/settings.patch
-if [ "`pwd`" == "/android/viperos/packages/apps/Settings" ]; then
-	patch -p1 < ../../../device/CUBOT/MAX/patches_mtk/settings_viper.patch
-fi
+patch -p1 < ../../../device/$brand/$model/patches_mtk/settings.patch
 git clean -f -d
 cd ../../..
 
 # EngineerMode Patches
 cd frameworks/av
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/engineermode/1-em.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/engineermode/1-em.patch
 cd ../..
 cd frameworks/base
-patch -p1 < ../../device/CUBOT/MAX/patches_mtk/engineermode/0002-em.patch
+patch -p1 < ../../device/$brand/$model/patches_mtk/engineermode/0002-em.patch
 cd ../..
 exit 0
