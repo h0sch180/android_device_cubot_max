e617ec9890aad021de2fa73ef098cbeeb6ffe546
h0sch180 <hoschi@anukis.de>
2019-07-04 23:59:57 +0200

    fix WifiDisplay

--- patches_mtk/apply-patches.sh
+++ patches_mtk/apply-patches.sh
@@ -17,11 +17,11 @@ cd ..
 cd system/sepolicy
 patch -p1 < ../../device/$brand/$model/patches_mtk/system_sepolicy.patch
 cd ../..
+cd system/netd
+patch -p1 < ../../device/$brand/$model/patches_mtk/system_netd.patch
+cd ../..
 
 ### DO NOT USE SYSTEM BLUETOOTH PATCH
-#cd system/netd
-#patch -p1 < ../../device/$brand/$model/patches_mtk/system_netd.patch
-#cd ../..
 #cd system/bt
 #patch -p1 < ../../device/$brand/$model/patches_mtk/system_bt.patch
 #cd ../..
